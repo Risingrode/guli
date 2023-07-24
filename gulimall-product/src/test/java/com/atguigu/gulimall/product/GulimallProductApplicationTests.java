4db5129966f5102a23211ab5491470d4a6e21eee
cwRising <3185087246@qq.com>
2023-07-24 22:07:15 +0800

    增加笔记

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -2,11 +2,15 @@ package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.additional.query.impl.QueryChainWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,10 +20,24 @@ public class GulimallProductApplicationTests {
 
     @Test
     public void contextLoads() {
-        BrandEntity brandEntity=new BrandEntity();
-        brandEntity.setName("小米");
-        brandService.save(brandEntity);
-        System.out.println("保存成功");
+//        BrandEntity brandEntity=new BrandEntity();
+//        brandEntity.setName("小米");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功");
+
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setName("小华为");
+//        brandService.updateById(brandEntity);
+//        System.out.println("修改成功");
+
+        List<BrandEntity> list= brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",1L));
+        list.forEach((item)->{
+            System.out.println(item);
+        });
+
     }
+
+
 
 }
