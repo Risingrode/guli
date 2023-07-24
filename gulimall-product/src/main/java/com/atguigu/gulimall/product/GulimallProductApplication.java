90e72a66c110b24426ffbff241351174ffba5e32
cwRising <3185087246@qq.com>
2023-07-24 17:06:09 +0800

    基本测试完成

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,5 +1,7 @@
 package com.atguigu.gulimall.product;
 
+import org.mapstruct.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 * 整合mybatis-plus
 */
 
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
