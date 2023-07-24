c6051176d1f957326dba05523e01b8f11a6661e5
cwRising <3185087246@qq.com>
2023-07-24 15:47:35 +0800

    P18错误修复

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -3,6 +3,10 @@ package com.atguigu.gulimall.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* 整合mybatis-plus
+*/
+
 @SpringBootApplication
 public class GulimallProductApplication {
 
