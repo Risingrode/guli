0c29e5f45b9eaa2c817c6988ddb4c6feae9c41c4
cwRising <3185087246@qq.com>
2023-07-27 13:02:41 +0800

    7-27

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.gateway;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// 1.开启服务注册发现
+// 2.配置nacos地址
+@EnableDiscoveryClient
+// 排除跟数据源有关的配置
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class,DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+}
