b7762f757f97c3160d7269be3eee7fdb4ba2c850
cwRising <3185087246@qq.com>
2023-08-26 11:14:30 +0800

    提交search模块

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/GulimallSearchApplication.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/GulimallSearchApplication.java
@@ -2,12 +2,8 @@ package com.atguigu.gulimall.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
-@EnableDiscoveryClient
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class GulimallSearchApplication {
 
     public static void main(String[] args) {
--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,21 +1,13 @@
 package com.atguigu.gulimall.search;
 
-import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallSearchApplicationTests {
-    @Autowired
-    public RestHighLevelClient client;
 
     @Test
     void contextLoads() {
-        System.out.println(client);
     }
 
 }
