1ccdd82ba93ba0b6869a1e6ccc0baa73faee29d1
Risingrode <3185087246@qq.com>
2023-10-08 12:49:59 +0800

    主页面可以正常动态显示

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -38,7 +38,9 @@ public class IndexController {
     @ResponseBody
     @GetMapping("/index/catalog.json")
     public Map<String, List<Catelog2Vo>> getCatelogJson(){
+
         return categoryService.getCatalogJson();
+
     }
 
 }
